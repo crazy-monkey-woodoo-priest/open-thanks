bbec014c6e48eb2627b6087ea4a97e0882366b1f
Tomas Valent <equivalent@eq8.eu>
2016-10-23 01:35:34 +0200

    thanks svg and receivers route rename

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :receivers
+  resources :r, controller: 'receivers', as: :receivers
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
